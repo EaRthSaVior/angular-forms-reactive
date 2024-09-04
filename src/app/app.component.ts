b7419beac8b2e70bcb48e16c26ab704dcaed4a7b
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-09-05 01:50:49 +0800

    custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,16 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  forbiddenNames = ['aaac', 'bbb'];
   signupForm: FormGroup;
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, [Validators.required]),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNamesValidator.bind(this),
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
@@ -30,5 +34,12 @@ export class AppComponent implements OnInit {
   }
   get hobbiesControls(): FormArray {
     return this.signupForm.get('hobbies') as FormArray;
+  }
+  forbiddenNamesValidator(formCtrl: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenNames.indexOf(formCtrl.value) !== -1) {
+      return { isForbiddenName: true };
+    } else {
+      return null;
+    }
   }
 }
